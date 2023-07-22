94d460c7bddac339fffa1572a877e505ce696bd6
doyhosae <doyhosae@gmail.com>
2023-07-22 20:38:14 +0900

    ver3.0

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 document.getElementById("calculateBtn").addEventListener("click", function() {
-    // Get input values
-    const totalHours = parseFloat(document.getElementById("totalHours").value);
-    const hourlyRate = parseFloat(document.getElementById("hourlyRate").value);
+  // Get input values
+  const totalHoursValue = document.getElementById("totalHours").value;
+  const hourlyRateValue = document.getElementById("hourlyRate").value;
   
-    // Calculate 주휴수당
-    let totalPay = 0;
-    if (totalHours >= 40) {
-      totalPay = totalHours * hourlyRate;
-    } else {
-      totalPay = (totalHours / 5) * hourlyRate;
-    }
+  // Check the validity of the inputs
+  if (!totalHoursValue || !hourlyRateValue || isNaN(totalHoursValue) || isNaN(hourlyRateValue)) {
+      alert("올바른 값을 입력해주세요.");
+      return;
+  }
   
-    // Format the result as KRW (Korean Won)
-    const formattedPay = new Intl.NumberFormat("ko-KR", { style: "currency", currency: "KRW" }).format(totalPay);
-  
-    // Display the result
-    const resultElement = document.getElementById("result");
-    resultElement.textContent = `주휴수당은 ${formattedPay}원 입니다.`;
+  const totalHours = parseFloat(totalHoursValue);
+  const hourlyRate = parseFloat(hourlyRateValue);
+
+  // Calculate 주휴수당
+  let totalPay = totalHours >= 40 ? totalHours * hourlyRate : (totalHours / 5) * hourlyRate;
+
+  // Format the result as KRW (Korean Won)
+  const formattedPay = new Intl.NumberFormat("ko-KR", { style: "currency", currency: "KRW" }).format(totalPay);
+
+  // Display the result
+  document.getElementById("result").textContent = `주휴수당은 ${formattedPay}원 입니다.`;
 });
